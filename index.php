64260483dd1a448f1c993136f18bc93ed9956c49
grigorenko.sam@student.greenriver.edu <samgree1@srv604.webhostingforstudents.com>
2023-05-11 12:10:17 -0400

    Adding all the base files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+
+require_once ('vendor/autoload.php');
+
+//Create and F3 (Fat Fre )
+$f3 = Base::instance();
+
+
+//definr a default route
+$f3->route('GET /', function($f3){
+    echo '<h2>Hello world!</h2>';
+
+    $view = new Template();
+    echo $view->render('view/pets.html');
+});
+
+
+
+
+//Run Fat Free
+$f3->run();
+
+
